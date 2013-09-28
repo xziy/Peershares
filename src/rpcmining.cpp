a43747899ee761897a25c331242d5566f9862cdf
defaced <defaced@copiamarket.com>
2013-09-28 14:09:02 -0400

    0.8.5.1 Updates

--- src/rpcmining.cpp
+++ src/rpcmining.cpp
@@ -25,17 +25,28 @@ Value GetNetworkHashPS(int lookup, int height) {
 
     // If lookup is -1, then use blocks since last difficulty change.
     if (lookup <= 0)
-        lookup = pb->nHeight % 720 + 1;
+        lookup = pb->nHeight % 2016 + 1;
 
     // If lookup is larger than chain, then set it to chain length.
     if (lookup > pb->nHeight)
         lookup = pb->nHeight;
 
-        CBlockIndex *pb0 = pb;
-    for (int i = 0; i < lookup; i++)
-    pb0 = pb0->pprev;
+    CBlockIndex *pb0 = pb;
+    int64 minTime = pb0->GetBlockTime();
+	int64 maxTime = minTime;
+    for (int i = 0; i < lookup; i++) {
+        pb0 = pb0->pprev;
+        int64 time = pb0->GetBlockTime();
+        minTime = std::min(time, minTime);
+        maxTime = std::max(time, maxTime);
+    }
+
+    // In case there's a situation where minTime == maxTime, we don't want a divide by zero exception.
+    if (minTime == maxTime)
+        return 0;
+
     uint256 workDiff = pb->nChainWork - pb0->nChainWork;
-    int64 timeDiff = pb->GetBlockTime() - pb0->GetBlockTime();
+    int64 timeDiff = maxTime - minTime;
 
     return (boost::int64_t)(workDiff.getdouble() / timeDiff);
 }
