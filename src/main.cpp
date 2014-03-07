8e2435525c5b278c76af967a298e6be310f71055
Michael Witrant <michael@ouvrages-web.fr>
2014-03-07 01:41:54 +0100

    start PoS at lower difficulty

--- src/main.cpp
+++ src/main.cpp
@@ -872,6 +872,14 @@ const CBlockIndex* GetLastBlockIndex(const CBlockIndex* pindex, bool fProofOfSta
     return pindex;
 }
 
+unsigned int static GetInitialTarget(bool fProofOfStake)
+{
+    if (fProofOfStake)
+        return CBigNum(~uint256(0) >> 20).GetCompact();
+    else
+        return bnInitialHashTarget.GetCompact();
+}
+
 unsigned int static GetNextTargetRequired(const CBlockIndex* pindexLast, bool fProofOfStake)
 {
     if (pindexLast == NULL)
@@ -879,10 +887,10 @@ unsigned int static GetNextTargetRequired(const CBlockIndex* pindexLast, bool fP
 
     const CBlockIndex* pindexPrev = GetLastBlockIndex(pindexLast, fProofOfStake);
     if (pindexPrev->pprev == NULL)
-        return bnInitialHashTarget.GetCompact(); // first block
+        return GetInitialTarget(fProofOfStake); // first block
     const CBlockIndex* pindexPrevPrev = GetLastBlockIndex(pindexPrev->pprev, fProofOfStake);
     if (pindexPrevPrev->pprev == NULL)
-        return bnInitialHashTarget.GetCompact(); // second block
+        return GetInitialTarget(fProofOfStake); // second block
 
     int64 nActualSpacing = pindexPrev->GetBlockTime() - pindexPrevPrev->GetBlockTime();
 
