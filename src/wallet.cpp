f5572721794a9a3ae96c67646d32ca09cf56b516
Michael Witrant <michael@ouvrages-web.fr>
2014-03-31 23:38:55 +0200

    allow the creation of larger transactions

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -1179,7 +1179,7 @@ bool CWallet::CreateTransaction(const vector<pair<CScript, int64> >& vecSend, CW
 
                 // Limit size
                 unsigned int nBytes = ::GetSerializeSize(*(CTransaction*)&wtxNew, SER_NETWORK, PROTOCOL_VERSION);
-                if (nBytes >= MAX_BLOCK_SIZE_GEN/5)
+                if (nBytes >= MAX_BLOCK_SIZE/4)
                     return false;
                 dPriority /= nBytes;
 
